f1887da179ce5ebebbf96b2dde33bf393741b2af
fulong <834424581@qq.com>
2018-04-26 17:42:41 +0800

    实现栈结构

--- src/Main.java
+++ src/Main.java
@@ -17,24 +17,32 @@ public class Main {
 //        for(int i =0;i<score.length; i++)
 //            System.out.println(score[i]);
 //    }
-        Array<Integer> arr = new Array<>();
-        for(int i=0;i<10;i++)
-            arr.addLast(i);
-        System.out.println(arr);
+//        Array<Integer> arr = new Array<>();
+//        for(int i=0;i<10;i++)
+//            arr.addLast(i);
+//        System.out.println(arr);
+//
+//        arr.add(1,100);
+//        System.out.println(arr);
+//
+//        arr.addFist(-1);
+//        System.out.println(arr);
+//
+//        arr.remove(2);
+//        System.out.println(arr);
+//
+//        arr.removeElement(4);
+//        System.out.println(arr);
+//
+//        arr.remveFirst();
+//        System.out.println(arr);
 
-        arr.add(1,100);
-        System.out.println(arr);
-
-        arr.addFist(-1);
-        System.out.println(arr);
-
-        arr.remove(2);
-        System.out.println(arr);
-
-        arr.removeElement(4);
-        System.out.println(arr);
-
-        arr.remveFirst();
-        System.out.println(arr);
+        ArrayStack<Integer> stack = new ArrayStack<>();
+        for(int i=0;i<5;i++){
+            stack.push(i);
+            System.out.println(stack);
+        }
+        stack.pop();
+        System.out.println(stack);
     }
 }
