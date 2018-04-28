22f451368e803f3af858748c5cd8c64fd9c3495a
fulong <834424581@qq.com>
2018-04-28 17:58:14 +0800

    链表实现栈的操作

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 import java.util.Random;
 public class Main {
     // 测试使用q运行opCount个enqueueu和dequeue操作所需要的时间，单位：秒
-    private static double testQueue(Queue<Integer> q, int opCount){
+//    private static double testQueue(Queue<Integer> q, int opCount){
+//
+//        long startTime = System.nanoTime();
+//
+//        Random random = new Random();
+//        for(int i = 0 ; i < opCount ; i ++)
+//            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+//        for(int i = 0 ; i < opCount ; i ++)
+//            q.dequeue();
+//
+//        long endTime = System.nanoTime();
+//
+//        return (endTime - startTime) / 1000000000.0;
+//    }
+    // 测试使用stack运行opCount个push和pop操作所需要的时间，单位：秒
+    private static double testStack(Stack<Integer> stack, int opCount){
 
         long startTime = System.nanoTime();
 
         Random random = new Random();
         for(int i = 0 ; i < opCount ; i ++)
-            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+            stack.push(random.nextInt(Integer.MAX_VALUE));
         for(int i = 0 ; i < opCount ; i ++)
-            q.dequeue();
+            stack.pop();
 
         long endTime = System.nanoTime();
 
@@ -61,15 +76,27 @@ public class Main {
 //        }
 //        stack.pop();
 //        System.out.println(stack);
+//        int opCount = 100000;
+//
+//        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+//        double time1 = testQueue(arrayQueue, opCount);
+//        System.out.println("ArrayQueue, time: " + time1 + " s");
+//
+//        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+//        double time2 = testQueue(loopQueue, opCount);
+//        System.out.println("LoopQueue, time: " + time2 + " s");
+
         int opCount = 100000;
 
-        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
-        double time1 = testQueue(arrayQueue, opCount);
-        System.out.println("ArrayQueue, time: " + time1 + " s");
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
 
-        LoopQueue<Integer> loopQueue = new LoopQueue<>();
-        double time2 = testQueue(loopQueue, opCount);
-        System.out.println("LoopQueue, time: " + time2 + " s");
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
+
+        // 其实这个时间比较很复杂，因为LinkedListStack中包含更多的new操作
     }
 
 }
