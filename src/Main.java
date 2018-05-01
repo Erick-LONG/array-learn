cdc5bb9089466133db67fe90e63d765267005733
fulong <834424581@qq.com>
2018-05-01 20:30:36 +0800

    二分搜素树前序遍历

--- src/Main.java
+++ src/Main.java
@@ -16,20 +16,20 @@ public class Main {
 //        return (endTime - startTime) / 1000000000.0;
 //    }
     // 测试使用stack运行opCount个push和pop操作所需要的时间，单位：秒
-    private static double testStack(Stack<Integer> stack, int opCount){
-
-        long startTime = System.nanoTime();
-
-        Random random = new Random();
-        for(int i = 0 ; i < opCount ; i ++)
-            stack.push(random.nextInt(Integer.MAX_VALUE));
-        for(int i = 0 ; i < opCount ; i ++)
-            stack.pop();
-
-        long endTime = System.nanoTime();
-
-        return (endTime - startTime) / 1000000000.0;
-    }
+//    private static double testStack(Stack<Integer> stack, int opCount){
+//
+//        long startTime = System.nanoTime();
+//
+//        Random random = new Random();
+//        for(int i = 0 ; i < opCount ; i ++)
+//            stack.push(random.nextInt(Integer.MAX_VALUE));
+//        for(int i = 0 ; i < opCount ; i ++)
+//            stack.pop();
+//
+//        long endTime = System.nanoTime();
+//
+//        return (endTime - startTime) / 1000000000.0;
+//    }
 
 
     public static void main(String[] args) {
@@ -86,17 +86,24 @@ public class Main {
 //        double time2 = testQueue(loopQueue, opCount);
 //        System.out.println("LoopQueue, time: " + time2 + " s");
 
-        int opCount = 100000;
-
-        ArrayStack<Integer> arrayStack = new ArrayStack<>();
-        double time1 = testStack(arrayStack, opCount);
-        System.out.println("ArrayStack, time: " + time1 + " s");
-
-        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
-        double time2 = testStack(linkedListStack, opCount);
-        System.out.println("LinkedListStack, time: " + time2 + " s");
+//        int opCount = 100000;
+//
+//        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+//        double time1 = testStack(arrayStack, opCount);
+//        System.out.println("ArrayStack, time: " + time1 + " s");
+//
+//        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+//        double time2 = testStack(linkedListStack, opCount);
+//        System.out.println("LinkedListStack, time: " + time2 + " s");
 
         // 其实这个时间比较很复杂，因为LinkedListStack中包含更多的new操作
+
+        BST<Integer> bst =new BST<>();
+        int[] nums = {5,3,6,8,4,2};
+        for(int num:nums)
+            bst.add(num);
+        bst.preOrder();
+        System.out.println(bst);
     }
 
 }
