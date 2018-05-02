62efe523523b0805207c7a4424d75f4a4fb60779
fulong <834424581@qq.com>
2018-05-02 13:14:16 +0800

    二分搜素树广度优先遍历

--- src/BST.java
+++ src/BST.java
@@ -1,4 +1,6 @@
+import java.util.LinkedList;
 import java.util.Stack;
+import java.util.Queue;
 public class BST<E extends Comparable<E>> {
 
     private class Node{
@@ -119,6 +121,20 @@ public class BST<E extends Comparable<E>> {
         postOrder(node.left);
         postOrder(node.right);
         System.out.println(node.e);
+    }
+
+    public void levelOrder(){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()){
+            Node cur = q.remove();
+            System.out.println(cur.e);
+            if(cur.left !=null)
+                q.add(cur.left);
+            if(cur.right !=null)
+                q.add(cur.right);
+        }
+
     }
 
     @Override
